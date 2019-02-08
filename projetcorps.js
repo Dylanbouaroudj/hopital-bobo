e9bcf1f5cf3b6b777078462901d6d235a41e60c4
Dylanbouaroudj <46968393+Dylanbouaroudj@users.noreply.github.com>
2019-02-08 16:44:42 +0100

    Add files via upload

---
+++ projetcorps.js
@@ -0,0 +1,49 @@
+   $('.epaules').click(function(){  
+    $('.patexte').hide();
+     var element =document.getElementById("texte1");
+      element.style.display = 'block';
+       $('#epaule').toggle();
+        });
+   
+
+
+   $('.coudes').click(function(){
+    $('.patexte').hide();
+     var element =document.getElementById("texte2");
+      element.style.display = 'block';
+       $('#coudes').toggle();  
+        });
+
+
+
+   $('.poignets').click(function(){ 
+   	$('.patexte').hide();
+     var element =document.getElementById("texte3");
+      element.style.display = 'block';
+       $('#poignets').toggle();  
+        });
+
+
+
+   $('.hanches').click(function(){ 
+   	$('.patexte').hide();
+     var element =document.getElementById("texte4");
+      element.style.display = 'block';
+       $('#hanches').toggle();  
+        });
+
+
+   $('.genoux').click(function(){ 
+   	$('.patexte').hide();
+     var element =document.getElementById("texte5");
+      element.style.display = 'block';
+       $('#genoux').toggle();  
+        });
+
+
+   $('.chevilles').click(function(){ 
+   	$('.patexte').hide();
+     var element =document.getElementById("texte6");
+      element.style.display = 'block';
+       $('#chevilles').toggle();  
+        });
